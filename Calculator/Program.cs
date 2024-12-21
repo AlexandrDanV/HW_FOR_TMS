a5b1c6e8b72edd6f128aeeb71b794cfd29b22d6c
SashaD <daniav1987@dmail.com>
2024-12-21 19:44:06 +0300

    Enter cod!

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool cont = true;
+            while (cont)
+            {
+                Console.Clear();
+                Console.WriteLine($"A mathematical example: (ex: 40*p20+44/8) where 'p' is percent!\n" +
+                    $"Start entering:");
+                var strIn = Console.ReadLine();
+                Console.WriteLine(new string('*', 50));
+                CalcClass calculator = new CalcClass(strIn);
+                var res = calculator.Calculater();
+                Console.WriteLine($"Result: {res}");
+                Console.WriteLine(new string('*', 50));
+                Console.WriteLine("to continue, click: Enter \n" +
+                    "to stop, click: 0 and Enter");
+                string number = Console.ReadLine();
+                {
+                    if (number == "0")
+                    {
+                        Console.Clear();
+                        cont = false;
+                    }
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
