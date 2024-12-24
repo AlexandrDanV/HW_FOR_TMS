1c4b785bd7e6595a0ab7c8225e0c6f1cd7d5e47d
SashaD <daniav1987@dmail.com>
2024-12-24 14:22:56 +0300

    Add cod for HW Presentstion

---
+++ Array_Present/MyArray.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Present
+{
+    internal class MyArray
+    {
+        Random random;
+        int[,] twoD_Array;
+
+        public MyArray(int[,] array)
+        {
+            twoD_Array = array;
+            random = new Random();
+        }
+        public void FillArray()
+        {
+            int lengthRow = twoD_Array.GetLength(0);
+            int lengthColumn = twoD_Array.GetLength(1);
+            {
+                for (int i = 0; i < lengthRow; i++)
+                {
+                    for (int j = 0; j < lengthColumn; j++)
+                    {
+                        twoD_Array[i, j] = random.Next(-99, 99);
+                    }
+                }
+            }
+        }
+        public int GetNegativeNum(int[,] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+        public int GetPositiveNum(int[,] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+        public int[,] SortArrayToUpper(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = j; k < array.GetLength(1); k++)
+                    {
+                        if (array[i, j] > array[i, k])
+                        {
+                            Swap(ref array[i, k], ref array[i, j]);
+
+                        }
+                    }
+
+
+
+                }
+            }
+            return array;
+        }
+        public int[,] SortArrayToLes(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = j; k < array.GetLength(1); k++)
+                    {
+                        if (array[i, j] < array[i, k])
+                        {
+                            Swap(ref array[i, k], ref array[i, j]);
+                        }
+                    }
+
+                }
+            }
+            return array;
+        }
+
+        public int[,] ReversELements(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int k = 0;
+                for (int j = array.GetLength(1) - 1; j >= array.GetLength(1) / 2; j--)
+                {
+
+                    var item = array[i, k];
+                    array[i, k] = array[i, j];
+                    array[i, j] = item;
+                    k++;
+                }
+            }
+            return array;
+        }
+        void Swap(ref int x, ref int y)
+        {
+            var z = x;
+            x = y;
+            y = z;
+        }
+    }
+}
---
+++ Array_Present/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Array_Present
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, column;
+            {
+                Console.Write("Введите количество строк массива: ");
+                var controlRow = int.TryParse(Console.ReadLine(), out row);
+                Console.Write("Введите количество столбцов массива: ");
+                var controlWidth = int.TryParse(Console.ReadLine(), out column);
+            }
+
+            int[,] twoD_Array = new int[row, column];
+            MyArray myArray = new MyArray(twoD_Array);
+            //заполняем
+            myArray.FillArray();
+            Console.WriteLine(new string('*', 50));
+            //выводим
+            for (int i = 0; i < twoD_Array.GetLength(0); i++)
+            {
+                for (int j = 0; j < twoD_Array.GetLength(1); j++)
+                {
+                    Console.Write($"{twoD_Array[i, j]} |");
+                }
+                Console.WriteLine();
+            }
+            // реверс элементов
+            Console.WriteLine(new string('*', 50));
+            myArray.ReversELements(twoD_Array);
+            for (int i = 0; i < twoD_Array.GetLength(0); i++)
+            {
+                for (int j = 0; j < twoD_Array.GetLength(1); j++)
+                {
+                    Console.Write($"{twoD_Array[i, j]} |");
+                }
+                Console.WriteLine();
+            }
+            //поиск отрицательных
+            var numNagative = myArray.GetNegativeNum(twoD_Array);
+            Console.WriteLine(new string('*', 50));
+            Console.WriteLine("Количество отрицательных : {0}", numNagative);
+            //поиск положительных
+            Console.WriteLine(new string('*', 50));
+            var numPositive = myArray.GetPositiveNum(twoD_Array);
+            Console.WriteLine("Количество положительных: {0}", numPositive);
+
+            Console.WriteLine(new string('*', 50));
+            // сортировка построчно от меньшего к большему
+            myArray.SortArrayToUpper(twoD_Array);
+            for (int i = 0; i < twoD_Array.GetLength(0); i++)
+            {
+                for (int j = 0; j < twoD_Array.GetLength(1); j++)
+                {
+                    Console.Write($"{twoD_Array[i, j]} |");
+                }
+                Console.WriteLine();
+            }
+
+            // сортировка построчно от большего к меньшему
+            Console.WriteLine(new string('*', 50));
+            myArray.SortArrayToLes(twoD_Array);
+            for (int i = 0; i < twoD_Array.GetLength(0); i++)
+            {
+                for (int j = 0; j < twoD_Array.GetLength(1); j++)
+                {
+                    Console.Write($"{twoD_Array[i, j]} |");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
