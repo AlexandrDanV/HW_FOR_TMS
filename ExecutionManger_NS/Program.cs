9707e5090b7b0991d70fb74aa81b5a099c09bd9e
SashaD <daniav1987@dmail.com>
2025-01-22 13:48:59 +0300

    add project

---
+++ ExecutionManger_NS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExecutionManger_NS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
