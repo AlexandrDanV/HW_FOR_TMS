ab38b7b50a63449c67464b19b8412a7ab484afd3
SashaD <daniav1987@dmail.com>
2025-01-25 22:21:30 +0300

    add code (without tests)

---
+++ StackGeneric/MyStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackGeneric
+{
+    internal class MyStack<T>
+    {
+        List<T> list = new List<T>();
+
+        public bool IsEmpty = true;
+        public void Push(T element)
+        {
+            list.Add(element);
+            Set_isEmpty();
+        }
+        public T Pop()
+        {
+            try
+            {
+                if (IsEmpty is true) { throw new Exception("The stack is empty"); }
+            }
+            catch (Exception) { throw; }
+
+            var item = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            Set_isEmpty();
+            return item;
+        }
+        public T Peek()
+        {
+            try
+            {
+                if (IsEmpty is true) { throw new Exception("The stack is empty"); }
+            }
+            catch (Exception) { throw; }
+
+            if (IsEmpty is true) { throw new Exception("The stack is empty"); }
+            return list[list.Count - 1];
+        }
+
+        public int Count()
+        {
+            return list.Count;
+        }
+        private void Set_isEmpty() => IsEmpty = (list.Count > 0) ? false : true;
+
+    }
+}
---
+++ StackGeneric/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StackGeneric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> myStack = new MyStack<int>();
+            myStack.Push(111);
+            myStack.Push(222);
+            myStack.Push(333);
+            Console.WriteLine($"The number of elements: {myStack.Count()}");
+            Console.WriteLine(new string('*', 50));
+
+            try
+            {
+                var elementPop = myStack.Pop();
+
+                Console.WriteLine($"Element: {elementPop}. " +
+                    $"The number of elements after Pop(): {myStack.Count()}");
+                Console.WriteLine(new string('*', 50));
+
+                var elementPeek = myStack.Peek();
+                Console.WriteLine($"Element: {elementPeek}. " +
+                    $"The number of elements after Peek(): {myStack.Count()}");
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
