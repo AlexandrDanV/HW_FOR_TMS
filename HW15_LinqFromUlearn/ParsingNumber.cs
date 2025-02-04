8da4f9bcecbd40471f1925545771838c6731c925
SashaD <daniav1987@dmail.com>
2025-02-04 22:51:22 +0300

    First task completed

---
+++ HW15_LinqFromUlearn/ParsingNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW15_LinqFromUlearn
+{
+    internal class ParsingNumber
+    {
+        public static int[] ParseNumbers(IEnumerable<string> lines)
+        {
+            return lines.Where(x => x != "")
+                .Select(x => (int.TryParse(x, out var resultInt)) ? resultInt : 0)
+                .ToArray();
+        }
+    }
+}
---
+++ HW15_LinqFromUlearn/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HW15_LinqFromUlearn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /// First task. 
+            foreach (var num in ParsingNumber.ParseNumbers(new[] { "-0", "+0000" }))
+                Console.WriteLine(num);
+            foreach (var num in ParsingNumber.ParseNumbers(new List<string> { "1", "", "-03", "0" }))
+                Console.WriteLine(num);
+            // Successful
+
+
+        }
+    }
+}
