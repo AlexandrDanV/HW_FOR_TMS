dae5885e611af558c0c125629fa56464f8701067
SashaD <daniav1987@dmail.com>
2025-02-05 09:19:11 +0300

    add second task

--- HW15_LinqFromUlearn/ParsingNumber.cs
+++ HW15_LinqFromUlearn/ParsingNumber.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 
 namespace HW15_LinqFromUlearn
 {
-    internal class ParsingNumber
+    internal class Parsing
     {
         public static int[] ParseNumbers(IEnumerable<string> lines)
         {
             return lines.Where(x => x != "")
                 .Select(x => (int.TryParse(x, out var resultInt)) ? resultInt : 0)
                 .ToArray();
+        }
+        public static List<Point> ParsePoints(IEnumerable<string> lines)
+        { 
+
+            return lines.Select(x => x.Split(" "))
+                        .Select(x => new Point(int.Parse(x[0]), int.Parse(x[1])))
+                        .ToList();
         }
     }
 }
--- HW15_LinqFromUlearn/Program.cs
+++ HW15_LinqFromUlearn/Program.cs
@@ -4,13 +4,18 @@
     {
         static void Main(string[] args)
         {
-            /// First task. 
-            foreach (var num in ParsingNumber.ParseNumbers(new[] { "-0", "+0000" }))
+            //first task:
+            Console.WriteLine("First task!");
+            foreach (var num in Parsing.ParseNumbers(new[] { "-0", "+0000" }))
                 Console.WriteLine(num);
-            foreach (var num in ParsingNumber.ParseNumbers(new List<string> { "1", "", "-03", "0" }))
+            foreach (var num in Parsing.ParseNumbers(new List<string> { "1", "", "-03", "0" }))
                 Console.WriteLine(num);
-            // Successful
-
+            //second task:
+            Console.WriteLine("Second task!");           
+            foreach (var point in Parsing.ParsePoints(new[] { "1 -2", "-3 4", "0 2" }))
+                Console.WriteLine(point.X + " " + point.Y);
+            foreach (var point in Parsing.ParsePoints(new List<string> { "+01 -0042" }))
+                Console.WriteLine(point.X + " " + point.Y);
 
         }
     }
